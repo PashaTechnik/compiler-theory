64932fb957579de494ab09de94968132b1b64c8c
Pasha Technik <pacha120431@gmail.com>
2021-04-27 23:37:18 +0300

    add MathCalculations

---
+++ compiler theory/MathСalculations.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace compiler_theory
+{
+    public class MathСalculations
+    {
+        public static double Simpson(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum1 = 0d;
+            var sum2 = 0d;
+            for (var k = 1; k <= n; k++)
+            {
+                var xk = a + k * h;
+                if (k <= n - 1)
+                {
+                    sum1 += f(xk);
+                }
+
+                var xk_1 = a + (k - 1) * h;
+                sum2 += f((xk + xk_1) / 2);
+            }
+
+            var result = h / 3d * (1d / 2d * f(a) + sum1 + 2 * sum2 + 1d / 2d * f(b));
+            return result;
+        }
+    }
+}
--- compiler theory/Program.cs
+++ compiler theory/Program.cs
@@ -6,6 +6,14 @@ namespace compiler_theory
     {
         static void Main(string[] args)
         {
+            Parser p = new Parser();
+            double q = Parser.CalculateExpression("(2 + 2) - (2 * 7)");
+            Console.WriteLine(q);
+            
+            double f(double x) => x * x / (x - 1);
+            var result = MathСalculations.Simpson(f, -1, 10, 1000);
+            Console.WriteLine("S = {0}", result);
+
         }
     }
 }
